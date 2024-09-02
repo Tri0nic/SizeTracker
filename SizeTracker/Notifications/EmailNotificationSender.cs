8f91431e0bf8fcdfd2924154e386d73444c5472c
Tri0nic <androyd0007@yandex.ru>
2024-09-03 01:31:51 +0300

    feat: отправка сообщений по email

--- SizeTracker/Notifications/EmailNotificationSender.cs
+++ SizeTracker/Notifications/EmailNotificationSender.cs
@@ -1,4 +1,6 @@
 ﻿using SizeTracker.Notifications;
+using System.Net.Mail;
+using System.Net;
 
 namespace SizeTracker.Notification
 {
@@ -6,6 +8,39 @@ namespace SizeTracker.Notification
     {
         public void SendNotification(string message)
         {
+            // Настройки SMTP-сервера Mail.ru
+            string smtpServer = "smtp.mail.ru"; //smpt сервер(зависит от почты отправителя)
+            int smtpPort = 587; // Обычно используется порт 587 для TLS
+            string smtpUsername = ""; //твоя почта, с которой отправляется сообщение
+            string smtpPassword = "";//пароль приложения (от почты)
+
+            // Создаем объект клиента SMTP
+            using (SmtpClient smtpClient = new SmtpClient(smtpServer, smtpPort))
+            {
+                // Настройки аутентификации
+                smtpClient.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+                smtpClient.EnableSsl = true;
+
+                using (MailMessage mailMessage = new MailMessage())
+                {
+                    mailMessage.From = new MailAddress(smtpUsername);
+                    mailMessage.To.Add(""); // Укажите адрес получателя
+                    mailMessage.Subject = "Заголовок сообщения (тема)";
+                    mailMessage.Body = $"Текст сообщения";
+
+                    try
+                    {
+                        // Отправляем сообщение
+                        smtpClient.Send(mailMessage);
+                        Console.WriteLine("Сообщение успешно отправлено.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Ошибка отправки сообщения: {ex.Message}");
+                    }
+                }
+            }
+
             // Логика отправки email-сообщения
             Console.WriteLine(message);
             Console.WriteLine("Уведомление отправлено на email");
